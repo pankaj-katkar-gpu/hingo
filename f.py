0b159f42402653fac67bca2a8142ca13e2288d57
apna_college <pankajkatkar263@gmail.com>
2026-05-30 00:15:14 +0530

    new_updated

--- f.py
+++ f.py
@@ -3,4 +3,10 @@ def add (a, b):
 
 def subtract (a, b):
     return a - b
-
+def multiply (a, b):
+    return a * b        
+def divide (a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero is not allowed."
